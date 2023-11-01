2c48dcb566d97fc8273bd7615c155100425625f0
akchonya <akchonyahokagivna@gmail.com>
2023-11-01 23:24:17 +0200

    redo ADMIN_IDS structure

--- core/handlers/start.py
+++ core/handlers/start.py
@@ -1,11 +1,10 @@
 '''
 /start greets a user
 '''
-import html 
 
-from aiogram import Router, html
+from aiogram import Router, html, F
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
 from aiogram.types import ReplyKeyboardRemove
 
 
@@ -14,7 +13,6 @@ start_router = Router()
 
 @start_router.message(CommandStart())
 async def start_handler(message: Message):
-
-        await message.answer(f"<b>привіт, {html.unparse(message.from_user.first_name)}!</b>",
-                            reply_markup=ReplyKeyboardRemove())
-
+    await message.answer(f"<b>привіт, {html.unparse(message.from_user.first_name)}!</b>",
+                        reply_markup=ReplyKeyboardRemove())
+        
--- core/utils/commands.py
+++ core/utils/commands.py
@@ -2,10 +2,9 @@ from aiogram import Bot
 from aiogram.types import BotCommand, BotCommandScopeDefault, BotCommandScopeChat
 from core.utils.config import ADMIN_ID
 
-ADMIN_ID = list(map(int, ADMIN_ID.split(", ")))
-MODER = int(ADMIN_ID[1])
-ADMIN = int(ADMIN_ID[0])
-# print(ADMIN_ID, ADMIN_ID[0], ADMIN_ID[1])
+
+MODER = ADMIN_ID[1]
+ADMIN = ADMIN_ID[0]
 
 user_commands = [
         BotCommand(
